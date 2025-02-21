60e93d3a2fdbdbe12c7fe89b3109c90db0c6a864
Imfae <95619108+Imfae@users.noreply.github.com>
2025-02-21 20:30:59 +0800

    Implement edit action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,16 +3,21 @@ class UsersController < ApplicationController
   end
 
   def new
+    @user = User.new
   end
 
   def create
     # @users = User.new(username: params[:username], email: params[:email], password: params[:password])
-    @users = User.new(user_params)
-    if @users.save
+    @user = User.new(user_params)
+    if @user.save
       redirect_to new_user_path
     else
-      render :new, :unprocessable_entity
+      render :new, status: :unprocessable_entity
     end
+  end
+
+  def edit
+    @user = User.find(params[:id])
   end
 
   private
